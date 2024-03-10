84124f7bf8566a7320ac8def1fbfed24df8f501d
shibowo <shadowslayer363@gmail.com>
2024-03-10 13:04:19 +0200

    Disabling sfe-movie dependent code in cmake.sh
    Typhon has(or at least appears to have) the ability to play videos on its own. However, that relies on sfe-movie, an old library that is outdated and unmaintained for a pretty long time. For now, this means that typhon has to be compiled without video(playing) support.

--- cmake.sh
+++ cmake.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DDEBUG=ON -DCMAKE_INSTALL_ICONDIR=/usr/share/pixmaps/ -DCMAKE_INSTALL_DESKTOPDIR=/usr/share/applications/ -DCMAKE_DATA_PATH=/usr/share/  
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DNOSFM=ON -DDEBUG=ON -DCMAKE_INSTALL_ICONDIR=/usr/share/pixmaps/ -DCMAKE_INSTALL_DESKTOPDIR=/usr/share/applications/ -DCMAKE_DATA_PATH=/usr/share/
